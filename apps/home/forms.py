fa7445a6f03c8260e38efa82b8a8680418503769
mario alberto <clcneogeek@gmail.com>
2014-11-13 00:39:07 -0600

    Agregando css al login

--- apps/home/forms.py
+++ apps/home/forms.py
@@ -1,8 +1,2 @@
 from django import forms
 
-
-class alumnoLoginForm(forms.Form):
-    no_control = forms.CharField()
-    contrasenia = forms.CharField()
-
-        
--- apps/home/urls.py
+++ apps/home/urls.py
@@ -4,5 +4,4 @@ urlpatterns = patterns('apps.home.views',
     url(r'^$', 'view_index', name='vista_principal'),
     url(r'^login/$', 'view_login', name='vista_loging'),
     url(r'^logout/$', 'view_logout', name='vista_logout'),
-    url(r'^login_alumnos/$', 'view_login_alumnos', name='vista_login_alumnos'),
 )
