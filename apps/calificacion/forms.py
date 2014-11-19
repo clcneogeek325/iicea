3a19ee9b67b36466bbaf3c72874d8cf208cff706
mario alberto <clcneogeek@gmail.com>
2014-11-19 00:03:22 -0600

    Validando datos que no se deben repetir

--- apps/calificacion/forms.py
+++ apps/calificacion/forms.py
@@ -8,4 +8,14 @@ class calificacionForm(forms.ModelForm):
 	class Meta:
 		model = calificacion
 		exclude = {'activo',}
-		
+
+	
+	"""
+	def clean_alumno(self):
+		a = self.cleaned_dat['alumno']
+		c = calificacion.objects.filter(alumno=self.alumno,materia=self.materia,semestre=self.semestre)
+		if c.exists():
+			print "si existe un"
+			raise forms.ValidationError("YA existe un registro con el mismo alumno,materia,semestre")
+		return a
+	"""
