be537f828fee047ecffe2f0fe620eeb78e07b90f
neogeek <clcneogeek@gmail.com>
2014-12-09 12:15:35 -0600

    corrigiendo detalles del chat

--- apps/chat/urls.py
+++ apps/chat/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import patterns, include, url
 from . import views
 
 urlpatterns = patterns('',
-    url(r'^chat/$', views.home, name='home'),
+    url(r'^chat/$', views.home, name='vista_chat'),
 )
